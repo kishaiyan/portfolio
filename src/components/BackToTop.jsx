3d525e0a531bca82a6a72650e9d2e2a83f8cff81
Kishaiyan <kishaiyanthangaraj@gmail.com>
2025-06-24 20:09:52 +0930

    Added more accessibility and seo practices

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -72,6 +72,10 @@ const BackToTop = ({ sections }) => {
 
   return (
     <button
+      aria-label={isUpArrow ? 'Scroll to top' : 'Scroll to next section'}
+      type="button"
+      title={isUpArrow ? 'Scroll to top':'Scroll to next section' 
+      }
       onClick={handleClick}
       className="fixed bottom-6 right-6 p-3 sm:p-4 rounded-full bg-gradient-to-r from-cyan-500 to-green-500 text-white shadow-lg hover:scale-110 transition-transform duration-300 z-50"
     >
